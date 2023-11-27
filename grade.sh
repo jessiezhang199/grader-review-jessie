0fcb3a5ffd9433adab8e8474afaaec4c28e6d62b
jessiezhang199 <jiz199@ucsd.edu>
2023-11-27 14:15:01 -0800

    push changes

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -7,6 +7,43 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+
+
+
+
+if [[ ! -f student-submission/ListExamples.java ]]
+then echo "Missing File"
+    exit 1
+fi
+
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/
+cd grading-area
+
+javac -cp . ListExamples.java
+if [ $? -ne 0 ]; then
+    echo "Error: Compilation of ListExamples.java failed."
+    exit 1
+fi
+
+javac -cp . your_grading_tests.java
+if [ $? -ne 0 ]; then
+    echo "Error: Compilation of grading tests failed."
+    exit 1
+fi
+
+javac -cp $CPATH ListExamples.java TestListExamples.java || {
+echo "Error"
+    exit 1
+}
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples 
+
+if [[ $? -ne 0 ]]
+then echo "0"
+else 
+echo "100"
+fi
 
 
 # Draw a picture/take notes on the directory structure that's set up after
